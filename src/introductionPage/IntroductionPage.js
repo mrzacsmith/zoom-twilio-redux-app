060838eb645399f043bd15610546ffb72808114a
Zac Smith <8953340+mrzacsmith@users.noreply.github.com>
2022-05-22 23:32:56 -0600

    👍 IMPROVE: set dispatch to false on introduction page

--- src/introductionPage/IntroductionPage.js
+++ src/introductionPage/IntroductionPage.js
@@ -1,8 +1,14 @@
 import './introduction-page.css'
 import logo from '../resources/images/logo.png'
 import ConnectingButtons from './ConnectingButtons'
+import { connect } from 'react-redux'
+import { setIsRoomHost } from '../store/actions'
+import { useEffect } from 'react'
 
-const IntroductionPage = () => {
+const IntroductionPage = ({ setIsRoomHostAction }) => {
+  useEffect(() => {
+    setIsRoomHostAction(false)
+  }, [])
   return (
     <div className='introduction_page_container'>
       <div className='introduction_page_panel'>
@@ -13,4 +19,10 @@ const IntroductionPage = () => {
   )
 }
 
-export default IntroductionPage
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setIsRoomHostAction: (isRoomHost) => dispatch(setIsRoomHost(isRoomHost)),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(IntroductionPage)
